910949c005fe0f10f3b8ced3bfe640bb6d05165a
Babar Baig <BobBaig@outlook.com>
2022-02-02 22:56:17 -0500

    Update account1.jsx

--- account1.jsx
+++ account1.jsx
@@ -1,5 +1,4 @@
 // this keeps a running total of deposits and withdrawals
-
 const ATMDeposit = ({ onChange }) => {
   return (
     <label className="label huge">
@@ -10,26 +9,29 @@ const ATMDeposit = ({ onChange }) => {
   );
 };
 
+// Account is a web component
 const Account = () => {
-  const [accountState, setAccountState] = React.useState(0);
+  const [accountState, setAccountState] = React.useState(0);    // note useState()
   var deposit = 0;
+
   const handleChange = event => {
     console.log(`handleChange ${event.target.value}`);
     deposit = Number(event.target.value);
   };
+
   const handleSubmit = event => {
     let newTotal = accountState + deposit;
-    alert(`Account total = ${newTotal}`);
+    // alert(`Account total = ${newTotal}`);
     setAccountState(newTotal);
     event.preventDefault();
   };
 
-  return (
+  return (      // return a form
     <form onSubmit={handleSubmit}>
       <h2>Account Balance {accountState}</h2>
       <ATMDeposit onChange={handleChange}> Deposit</ATMDeposit>
     </form>
   );
 };
-// ========================================
+
 ReactDOM.render(<Account />, document.getElementById("root"));
